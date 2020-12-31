6b50968e068fb3be035f77698f7edf6bd6e21538
AarohiBarua <73633463+AarohiBarua@users.noreply.github.com>
2020-12-31 09:26:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,79 +1,46 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Events = Matter.Events;
+const Bodies = Matter.Bodies;
  
-var particles = [];
-var plinkos = [];
+var particles;
+var plinko1,plinko2,plinko3,plinko4,plinko5;
+var plinko6,plinko7,plinko8,plinko9,plinko10;
+var division;
 
-var divisionHeight=300;
-var score =0;
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(width/2,height,width,20);
-
-
-   for (var k = 0; k <=width; k = k + 80) {
-     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-   }
-
-
-    for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,175));
-    }
-
-     for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,275));
-    }
-
-     for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,375));
-    }
-
-    
-
-    
+  ground = new Ground(400,height,800,20);
+  division = new Ground(150,305,10,300);
+  plinko1 = new Plinko(800,300,70,70);
+  plinko2 = new Plinko(800,300,70,70);
+  plinko3 = new Plinko(700,300,70,70);
+  plinko4 = new Plinko(600,300,70,70);
+  plinko5 = new Plinko(500,300,70,70);
+  plinko6 = new Plinko(400,300,70,70);
+  plinko7 = new Plinko(300,300,70,70);
+  plinko8 = new Plinko(200,300,70,70);
+  plinko9 = new Plinko(100,300,70,70);
+  plinko10 = new Plinko(100,300,70,70);
 }
- 
-
 
 function draw() {
   background("black");
   textSize(20)
  //text("Score : "+score,20,30);
   Engine.update(engine);
- 
-  
-   for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
-   if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
-     score++;
-   }
- 
-  for (var j = 0; j < particles.length; j++) {
-   
-     particles[j].display();
-   }
-   for (var k = 0; k < divisions.length; k++) {
-     
-     divisions[k].display();
-   }
+  division.display();
+  ground.display();
+  plinko1.display();
+  plinko2.display();
+  plinko3.display();
+  plinko4.display();
+  plinko5.display();
+  plinko6.display();
+  plinko7.display();
+  plinko8.display();
+  plinko9.display();
+  plinko10.display();
 }
